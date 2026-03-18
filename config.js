b3f8b35597376d7f13041aa4891ec0da9d684480
Lakshitha-1629 <wijendraf5@gmail.com>
2026-03-18 10:19:02 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,30 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Lakshitha-1629/1629/blob/main/logo/alive.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "╔══════════════════════════════════╗
+        ☠️ 𝗡𝗢𝗠𝗘𝗥𝗖𝗬 – 𝗠𝗗 ☠️
+           SYSTEM STATUS
+╚══════════════════════════════════╝
+
+🤖 BOT NAME   : NoMercy-MD
+👑 OWNER      : Lakshitha
+⚙ VERSION    : V1
+🛡 MODE       : PRIVATE
+
+━━━━━━━━━━━━━━━━━━━━
+⚡ SYSTEM ONLINE
+💀 ALL MODULES ACTIVE
+🔥 READY TO EXECUTE COMMANDS
+━━━━━━━━━━━━━━━━━━━━
+
+🧠 AI SYSTEM : ACTIVE
+📡 SERVER    : CONNECTED
+🔐 SECURITY  : ENABLED
+
+☠️ NO MERCY | NO LIMIT | NO FEAR
+💀 OUT OF METRIX CYBER TEAM",
+BOT_OWNER: '94751593021',  // Replace with the owner's phone number
 
 
 
